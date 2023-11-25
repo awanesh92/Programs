90ef436e10b51c9a853ac380e4a28de79619952e
awanesh92 <yawanesh@gmail.com>
2023-11-25 07:20:40 +0530

    CodeForces Solution for 1352A

---
+++ PythonInterviewSols/CodeForces/SumOfRoundNumber.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+  n = int(input())
+  ans = []
+  power = 1
+
+  while n > 0:
+    if n % 10 > 0:
+      ans.append((n % 10) * power)
+    n //= 10
+    power *= 10
+
+  print(len(ans))
+  print(*ans)
